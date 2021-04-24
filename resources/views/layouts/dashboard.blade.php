e4bea1cd0d655a19111543ce5f5069058777ab56
JeganKannan1 <jegancs0215@gmail.com>
2021-04-25 00:26:32 +0530

    design

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,10 +7,10 @@
 		<meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects">
         <meta name="author" content="Dreamguys - Bootstrap Admin Template">
         <meta name="robots" content="noindex, nofollow">
-        <title>Dashboard - HRMS admin template</title>
+        <title>Team Lead</title>
 		
 		<!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{asset('/assets/img/favicon.png')}}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('/assets/img/sparklogo.png')}}">
 		
 		<!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{asset('/assets/css/bootstrap.min.css')}}">
@@ -43,33 +43,33 @@
 			
 				<!-- Logo -->
                 <div class="header-left">
-                    <a href="index.html" class="logo" style="margin: 10px">
-						<img src="{{asset('/assets/img/logo.png')}} " width="40" height="40" alt="">
+                    <a href="index.html" class="logo">
+						<img src="{{asset('/assets/img/sparkout.png')}}" alt="">
 					</a>
                 </div>
 				<!-- /Logo -->
 				
-				<a id="toggle_btn" href="javascript:void(0);" style="margin: 20px">
+				<!-- <a id="toggle_btn" href="javascript:void(0);" style="margin: 20px">
 					<span class="bar-icon">
 						<span></span>
 						<span></span>
 						<span></span>
 					</span>
-				</a>
+				</a> -->
 				
 				<!-- Header Title -->
-                <div class="page-title-box">
+                <!-- <div class="page-title-box">
 					<h3>Sparkout Tech</h3>
-                </div>
+                </div> -->
 				<!-- /Header Title -->
 				
-				<a id="mobile_btn" class="mobile_btn" href="#sidebar"><i class="fa fa-bars"></i></a>
+				<a id="mobile_btn" class="mobile_btn" href="#sidebar/teamsidebar"><i class="fa fa-bars"></i></a>
 				
 				<!-- Header Menu -->
-				<ul class="nav user-menu">
+				<!-- <ul class="nav user-menu">
 				
-					<!-- Search -->
-					<li class="nav-item">
+					Search -->
+					<!-- <li class="nav-item">
 						<div class="top-nav-search">
 							<a href="javascript:void(0);" class="responsive-search">
 								<i class="fa fa-search"></i>
@@ -79,11 +79,11 @@
 								<button class="btn" type="submit"><i class="fa fa-search"></i></button>
 							</form>
 						</div>
-					</li>
+					</li> -->
 					<!-- /Search -->
 				
 					<!-- Flag -->
-					<li class="nav-item dropdown has-arrow flag-nav">
+					<!-- <li class="nav-item dropdown has-arrow flag-nav">
 						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">
 							<img src="{{asset('assets/img/flags/us.png')}}" alt="" height="20"> <span>English</span>
 						</a>
@@ -101,11 +101,11 @@
 								<img src="{{asset('/assets/img/flags/de.png')}} " alt="" height="16"> German
 							</a>
 						</div>
-					</li>
+					</li> -->
 					<!-- /Flag -->
 				
 					<!-- Notifications -->
-					<li class="nav-item dropdown">
+					<!-- <li class="nav-item dropdown">
 						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
 							<i class="fa fa-bell-o"></i> <span class="badge badge-pill">3</span>
 						</a>
@@ -187,11 +187,11 @@
 								<a href="activities.html">View all Notifications</a>
 							</div>
 						</div>
-					</li>
+					</li> -->
 					<!-- /Notifications -->
 					
 					<!-- Message Notifications -->
-					<li class="nav-item dropdown">
+					<!-- <li class="nav-item dropdown">
 						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
 							<i class="fa fa-comment-o"></i> <span class="badge badge-pill">8</span>
 						</a>
@@ -293,9 +293,9 @@
 								<a href="chat.html">View all Messages</a>
 							</div>
 						</div>
-					</li>
-					<!-- /Message Notifications -->
-
+					</li>  -->
+					 <!-- /Message Notifications -->
+z
 					<li class="nav-item dropdown has-arrow main-drop">
 						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
 							<span class="user-img"><img src="{{asset('/assets/img/profiles/avatar-21.jpg')}} " alt="">
--- resources/views/sidebar/usersidebar.blade.php
+++ resources/views/sidebar/usersidebar.blade.php
@@ -3,24 +3,9 @@
     <div class="sidebar-inner slimscroll">
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
-                <li class="menu-title"> 
-                    <span>Main</span>
-                </li>
-                <li class="submenu">
-                    <a href="#"><i class="la la-dashboard"></i> <span> Dashboard</span> <span class="menu-arrow"></span></a>
-                    <ul style="display: none;" >
-                        <li><a class="active" href="/dashboard">employee Dashboard</a></li>
-                        
-                    </ul>
-                </li>
-                <li class="submenu">
-                    <a href="#"><i class="la la-cube"></i> <span> Pages</span> <span class="menu-arrow"></span></a>
-                    <ul style="display: none;">
-                        <li><a href="/take-test">take Test</a></li>
-                        <li><a href="/report">Report</a></li>
-                     </ul>
-                        
-                </li>
+                <li><a class="active" href="/dashboard"><i class="la la-cube">Employee Dashboard</i></a></li>
+                <li><a href="/take-test"><i class="la la-cube">Take Test</i></a></li>
+                <li><a href="/report"><i class="la la-cube">Report</i></a></li>
             </ul>
         </div>
     </div>
